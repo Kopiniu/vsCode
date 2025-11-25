0d245fa11aed0b76722dde6e5f29068fe20897b9
Kopiniu <dawidfigorekk@gmail.com>
2025-11-25 23:04:02 +0100

    25.11.2025

---
+++ cpp/double.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    /* "Int" to skrót od "integer" (liczba całkowita)
+     "Double" to skrót od "double precision floating point number" (liczba zmiennoprzecinkowa podwójnej precyzji)
+     czyli liczby ułamki dziesiętne */
+
+    double pi {3.14159};
+    double radius {5.0};
+
+    cout << pi << " " << radius << endl;
+    return 0;
+}
---
+++ cpp/pKalkulator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a {}, b {};
+
+    cout << "Wpisz pierwszą liczbę:";
+    cin >> a;
+    cout << "Wpisz drugą liczbę:";
+    cin >> b;
+
+    cout << "Twój wynik to: " << a+b << endl;
+    return 0;
+}
